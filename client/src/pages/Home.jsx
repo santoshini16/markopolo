f25ebac7951d565e490d32f0cf73b460f7854ab8
santoshini <santoshinipradhan01@gmail.com>
2025-01-24 17:00:12 +0530

    home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 
 const Home = () => {
   return (
-    <div>
-      hello
+    <div className={styles.homepage}>
+      <img src="/orbital.png" alt="orbital" className={styles.orbital} />
+      <div className={styles.left}>
+        <h1>SHOPTALK</h1>
+        <h2>Supercharge your products</h2>
+        <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam aliquam distinctio asperiores molestias magnam, eaque natus, quam numquam cumque accusantium blanditiis at facere delectus. Mollitia pariatur voluptatibus magnam assumenda enim.</h3>
+        <button>Upload your documents</button>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.imageContainer}>
+          <div className={styles.bgContainer}>
+            <div className={styles.bg}></div>
+          </div>
+          <img src="/bot.png" alt="" className={styles.bot} />
+        </div>
+      </div>
     </div>
   )
 }

a70d214bb552cbef8d67823881b85a2db1e8e704
santoshini <santoshinipradhan01@gmail.com>
2025-01-25 13:32:28 +0530

    backend initiated

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { mongoose } = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+const app = express();
+const PORT = 3000;
+
+// CORS setup for allowing requests from your frontend
+app.use(cors({
+    origin: ['http://localhost:5173',process.env.CLIENT_URL],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+app.options('*', cors());
+// Body parsing middleware (Express built-in)
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+  
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((error) => {
+        console.log('Failed to connect to MongoDB:', error);
+    });
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+});
